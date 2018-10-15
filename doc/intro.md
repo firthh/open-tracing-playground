b673f29e46bc10d1bec9ea399baecfc1f8cd63e8
Hugo Firth <hugo.firth@gmail.com>
2018-10-15 13:26:32 +0100

    Some basic tracing working

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tracing-example
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/tracing_example/core.clj
@@ -0,0 +1,38 @@
+(ns tracing-example.core
+  (:gen-class)
+  (:import io.jaegertracing.Configuration
+           [io.opentracing Tracer Span Scope]))
+
+;; Tracing
+
+(def ^Tracer tracer (delay (.getTracer (Configuration/fromEnv))))
+
+(defmacro span [^String span-name body]
+  `(with-open [^Scope scope# (.startActive (.buildSpan @tracer ~span-name) true)]
+     ~body))
+
+;; Batch processing
+
+(defn get-messages []
+  (Thread/sleep 1000)
+  (range (rand-int 100)))
+
+(defn process-messages [messages]
+  (map (fn [message] (Thread/sleep (rand-int 100)) message) messages))
+
+(defn publish-messages [messages]
+  (Thread/sleep 500))
+
+;; Putting all together
+
+(defn -main [& args]
+  (prn @tracer)
+  (while true
+    (do
+      (println "Processing batch")
+      (span "process-batch"
+            (-> (get-messages)
+                (process-messages)
+                (publish-messages)))
+      (println "Sleeping")
+      (Thread/sleep 5000))))
